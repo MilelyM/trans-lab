3d4f32e25089b3d01270e4f3278c2671c64028e5
milelym <milelymarcial@gmail.com>
2018-07-02 23:01:01 -0400

    funcion de saldo solucionando error

--- js/main2.js
+++ js/main2.js
@@ -1,19 +1,19 @@
 document.getElementById('btnsaldo').addEventListener('click',carga);
-let tarjeta=document.getElementById('numerot').value
 //let strtarjeta=tarjeta.toString();
 
 
 function carga(){
- url='http://www.psep.cl/api/Bip.php?&numberBip=89515804' ;
+    let tarjeta=document.getElementById('numerot').value
+
+ url=`http://www.psep.cl/api/Bip.php?&numberBip=${tarjeta}` ;
     fetch(url)
-    
         .then(function(resp) {
             return resp.json();
   
         })
         .then(function (valores) {
         let data = Object.values(valores)
-        // console.log(valores);
+        console.log(valores);
         // let numeroTarjeta = data[0];
          let estadoContrato = data[1];
          let saldoTarjeta = data[2]
@@ -23,8 +23,6 @@ function carga(){
            // console.log(saldoTarjeta);
             document.getElementById('cajaDeTexto').innerHTML= saldoTarjeta;
             
-
-        
         })
 
        // .catch(e => console.log(e))
