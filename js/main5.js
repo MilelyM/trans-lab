18e580fb751cfc5621091101b56a506e32643956
milelym <milelymarcial@gmail.com>
2018-07-08 19:02:10 -0400

    prueba

--- js/main5.js
+++ js/main5.js
@@ -8,7 +8,7 @@ document.getElementById('btnsaldo').addEventListener('click',carga);
 function carga(){
     let tarjeta=document.getElementById('numerot').value
 
-    fetch(`https://www.psep.cl/api/Bip.php?&numberBip=${tarjeta}`)
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${tarjeta}`)
         .then(function(resp) {
             return resp.json();
   
